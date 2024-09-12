6a9d1110a1493ee42d207f800080d33753e7f5c2
Brian Ali <gamerfunky78@gmail.com>
2024-09-12 13:49:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const observer = new IntersectionObserver(entries => {
     } else {
       NAV2.style.display = 'none'; // Hide navbar
       upToTop.style.display = 'flex';//hide uptotop button.
-    } 
+    }
   });
 });
 
@@ -75,54 +75,54 @@ async function getVideoGames() {
 
   const corsProxyUrl = "https://corsproxy.io/?"
   const url = 'https://www.gamerpower.com/api/giveaways'
-  
+
 
   const finishedUrl = corsProxyUrl + url + choice
 
   try {
-    const response = await fetch(finishedUrl,{ cache: 'no-cache' }); 
+    const response = await fetch(finishedUrl, { cache: 'no-cache' });
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const data = await response.json();
-    const gameCardsContainer = document.getElementById("IMAGES-GIVE-AWAY"); 
-    
+    const gameCardsContainer = document.getElementById("IMAGES-GIVE-AWAY");
+
 
     data.forEach(game => {
       const card = document.createElement('div');
       card.classList.add('Game-card');
-      
+
       const image = document.createElement('img');
       const button = document.createElement('button');
-button.classList.add('Game-button');
+      button.classList.add('Game-button');
 
-const gameLink = document.createElement('a');  
+      const gameLink = document.createElement('a');
 
-// Set button text content (optional)
-button.textContent = "Get Now"; 
+      // Set button text content (optional)
+      button.textContent = "Get Now";
 
-// Add event listener for mouseover
-button.addEventListener('mouseover', () => {
-  button.style.backgroundColor = "#008B8B";
-  button.style.color = "black"
-  button.style.width = "100%"
-  button.textContent = "Get now →"; 
-});
+      // Add event listener for mouseover
+      button.addEventListener('mouseover', () => {
+        button.style.backgroundColor = "#008B8B";
+        button.style.color = "black"
+        button.style.width = "100%"
+        button.textContent = "Get now →";
+      });
 
-button.addEventListener('mouseout', () => {
-  button.style.backgroundColor = ""; // Reset to default color
-  button.style.color = "white"
-  button.textContent = "Get Now"; 
-});
+      button.addEventListener('mouseout', () => {
+        button.style.backgroundColor = ""; // Reset to default color
+        button.style.color = "white"
+        button.textContent = "Get Now";
+      });
 
-//variables declarations//
-const description = document.createElement('p');
-const AccessInfo = document.createElement('p');
-const AcessDiv = document.createElement('div');
-const Worth = document.createElement('p');
-const GameTitle = document.createElement('p');
+      //variables declarations//
+      const description = document.createElement('p');
+      const AccessInfo = document.createElement('p');
+      const AcessDiv = document.createElement('div');
+      const Worth = document.createElement('p');
+      const GameTitle = document.createElement('p');
 
 
       button.style.backgroundColor = 'transparent';
@@ -195,14 +195,14 @@ const GameTitle = document.createElement('p');
       card.style.padding = "5px"
       function adjustCardHeight() {
         if (window.innerWidth < 768) {
-          card.style.height = "fit-content"; 
+          card.style.height = "fit-content";
         } else {
-          card.style.height = "500px"; 
+          card.style.height = "500px";
         }
       }
       adjustCardHeight();
       window.addEventListener("resize", adjustCardHeight);
-      card.style.overflowY = "scroll" 
+      card.style.overflowY = "scroll"
       card.style.scrollbarWidth = "none"
       card.style.position = "relative"
       card.style.border = "none"
@@ -211,12 +211,12 @@ const GameTitle = document.createElement('p');
 
       card.addEventListener('mouseover', () => {
         card.style.backgroundColor = "#3b3a3b"
-        card.style.boxShadow = "0px 4px 5px 1px #008B8B, 0px 6px 20px 10px rgba(255, 255, 255, 0.15)" 
+        card.style.boxShadow = "0px 4px 5px 1px #008B8B, 0px 6px 20px 10px rgba(255, 255, 255, 0.15)"
       });
       card.addEventListener('mouseout', () => {
         card.style.backgroundColor = "#2F2D2E"
-        card.style.boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.9)" 
-      });      
+        card.style.boxShadow = "0px 2px 4px rgba(0, 0, 0, 0.9)"
+      });
     });
   } catch (error) {
     console.error('Error fetching Giveaways:', error);
@@ -274,24 +274,24 @@ function toggleContacts() {
 }
 
 
-async function EstimateWorth(){
+async function EstimateWorth() {
 
   let WorthMessage = document.getElementById("games-worth")
   let NoOfGiveaways = document.getElementById("games-no")
 
   const url = 'https://gamerpower.com/api/worth/'
   const corsProxyUrl = "https://api.codetabs.com/v1/proxy?quest="
-  const finishedUrl = corsProxyUrl+url
+  const finishedUrl = corsProxyUrl + url
 
   try {
 
-    const response = await fetch(finishedUrl,{ cache: 'no-cache' }); 
+    const response = await fetch(finishedUrl, { cache: 'no-cache' });
     const data = await response.json();
     WorthMessage.textContent = `Save on a total of $ ${data.worth_estimation_usd}`
     WorthMessage.style.color = "black"
     WorthMessage.style.fontFamily = "josefin sans, impact, monospace"
     WorthMessage.style.fontSize = "30px"
-    
+
 
     NoOfGiveaways.textContent = `Active giveaways Today: ${data.active_giveaways_number}.`
     NoOfGiveaways.style.color = "black"
@@ -303,7 +303,7 @@ async function EstimateWorth(){
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    
+
   } catch (error) {
     console.error('Error fetching games worth:', error);
 
@@ -328,30 +328,64 @@ function toggleSorter() {
   }
 }
 
-function sortBydate(){
-    document.getElementById("IMAGES-GIVE-AWAY").innerHTML = ""
-    choice = "?sort-by=date";
-    getVideoGames();
+function sortBydate() {
+  document.getElementById("IMAGES-GIVE-AWAY").innerHTML = ""
+  choice = "?sort-by=date";
+  getVideoGames();
 }
-function sortByValue(){
-    document.getElementById("IMAGES-GIVE-AWAY").innerHTML = ""
-    choice = "?sort-by=value";
-    getVideoGames();
+function sortByValue() {
+  document.getElementById("IMAGES-GIVE-AWAY").innerHTML = ""
+  choice = "?sort-by=value";
+  getVideoGames();
 }
-function sortByPopularity(){
-    document.getElementById("IMAGES-GIVE-AWAY").innerHTML = ""
-    choice = "?sort-by=popularity";
-    getVideoGames();
+function sortByPopularity() {
+  document.getElementById("IMAGES-GIVE-AWAY").innerHTML = ""
+  choice = "?sort-by=popularity";
+  getVideoGames();
 }
-function sortByAll(){
+function sortByAll() {
   document.getElementById("IMAGES-GIVE-AWAY").innerHTML = ""
   choice = "";
   getVideoGames();
 }
 
-function toggleCookieBar(){
-    document.querySelector(".COOKIE_BAR").style.display = "none"
+function toggleCookieBar() {
+  document.querySelector(".COOKIE_BAR").style.display = "none"
 }
 
+
+function adminShow(){
+  let ADMIN = document.getElementById("Admin_Login")
+
+  if(ADMIN.style.display == "none" || ADMIN.style.display == ""){
+     ADMIN.style.display = "flex"
+  }
+  else{
+    ADMIN.style.display = "none"
+  }
+    
+}
+
+function uploadShow(){
+    let ADMIN = document.getElementById("Admin_Login")
+    let UPLOADER = document.getElementById("adminUploads")
+    let ADT = document.getElementById("AdminToggler")
+    ADMIN.style.display = "none"
+    UPLOADER.style.display = "flex"
+    ADT.style.display = "none"
+}
+
+
+function auth(){
+  let username = document.getElementById("USERNAME")
+  let password = document.getElementById("PASSWD")
+  
+  if (username.value === "admin1" && password.value === "password1"){
+      uploadShow()
+  }
+  else{
+    window.alert("Wrong Password!!!")
+  }
+}
 
 
